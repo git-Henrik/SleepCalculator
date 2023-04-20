bef69070e2216c697ad827405d6531d2b1b95516
henrikDavin <davin.h.github.com>
2023-04-20 13:46:41 -0400

    first commit

---
+++ BetterBest/BetterBestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterBestApp.swift
+//  BetterBest
+//
+//  Created by Davin Henrik on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterBestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
